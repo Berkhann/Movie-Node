39fac68ee2a376421e8b4a41b8ecc4974fc31fe4
Berkhan <berkhanhalacoglu@hotmail.com>
2020-04-15 01:03:27 +0300

    C4:Adding Update,delete,top10,find endpoints

--- models/Movie.js
+++ models/Movie.js
@@ -11,7 +11,7 @@ const MovieSchema = new Schema({
     country:String,
     year:Number,
     director_id: Schema.Types.ObjectId,
-    date:{
+    createdDate:{
         type:Date,
         default:Date.now
     }
--- routes/movie.js
+++ routes/movie.js
@@ -5,17 +5,88 @@ var router = express.Router();
 const Movie = require('../models/Movie');
 
 
+//End Points
+
+router.get('/top10',(req,res)=>{
+  const promise = Movie.find({ }).limit(10).sort({ imdb_score: -1 });
+
+  promise.then((data)=>{
+    res.json(data);
+  }).catch((err)=>{
+    res.json(err);
+  });
+});
+
+
+
+router.delete('/:movie_id',(req,res,next)=>{//delete endpoint
+  const promise = Movie.findByIdAndRemove(req.params.movie_id,req.body);//req.params içinde gelen parametreleri bulundurur
+
+  promise.then((movie)=>{
+    if(!movie)
+      next({message: 'The movie was not found'});
+    
+    res.json({movie});
+  }).catch((err)=>
+  {
+    res.json(err);
+  });
+});
+
+router.put('/:movie_id',(req,res,next)=>{//update endpoint
+  const promise = Movie.findByIdAndUpdate(
+    req.params.movie_id,
+    req.body,
+    {
+      new:true//update edildikten sonra yeni datanın dönülmesini saglar
+    });//req.params içinde gelen parametreleri bulundurur
+
+  promise.then((movie)=>{
+    if(!movie)
+      next({message: 'The movie was not found'});
+    
+    res.json({movie});
+  }).catch((err)=>
+  {
+    res.json(err);
+  });
+});
+
+
+
+router.get('/:movie_id',(req,res,next)=>{//id ile bulma endpoint
+    const promise = Movie.findById(req.params.movie_id);//req.params içinde gelen parametreleri bulundurur
+
+    promise.then((movie)=>{
+      if(!movie)
+        next({message: 'The movie was not found'});
+      
+      res.json(movie);
+    }).catch((err)=>
+    {
+      res.json(err);
+    });
+});
+
+router.get('/',(req,res)=>{//All movies
+    const promise = Movie.find({ });
+
+    promise.then((data)=>{
+      res.json(data);
+    }).catch((err)=>{
+      res.json(err);
+    });
+});
+
 router.post('/', function(req, res, next) {
   //const {title,imdb_score,category,country,year} = req.body;
   const movie = new Movie(req.body);
-
  /* movie.save((err,data)=>{
     if(err)
       res.json(err);
 
     res.json({status:1});
   });*/
-
   const promise = movie.save();
   promise.then((data)=>{
     res.json({status:1})
@@ -23,5 +94,6 @@ router.post('/', function(req, res, next) {
     res.json(err);
   });
 });
+
 
 module.exports = router;
